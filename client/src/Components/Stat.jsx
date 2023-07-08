23d810fa4b312f851f775b4ac0e59ff55e638885
Saqlain451 <jobs.sakil.mustak@gmail.com>
2023-07-08 12:38:07 +0530

    profile photo added

--- client/src/Components/Stat.jsx
+++ client/src/Components/Stat.jsx
@@ -3,11 +3,8 @@ import React from 'react';
 const Stat = () => {
     return (
         <>
-            {/*<div className="d-flex mt-3">*/}
-            {/*    <span className="pt-1 pb-1 pl-3 pr-3 bg-secondery br-0-5 text-green ls-1">📊 GITHUB STATS:</span>*/}
-            {/*</div>*/}
 
-            <div className="grid-3 g-1 mt-2 grid-md-2 grid-sm-1 stat">
+            <div className="grid-3 g-1 mt-4 grid-md-2 grid-sm-1  stat">
                 <img
                     src="https://github-readme-stats.vercel.app/api/top-langs/?username=Saqlain451&theme=transparent&show_icons=true&hide_border=false&include_all_commits=false&count_private=false&layout=compact"
                     alt="github stat" className={"img-fluid"}/>
